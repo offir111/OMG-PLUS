ebf2ab512eb08a0204a17faee608f5e6cbc3c633
offir111 <offir111@gmail.com>
2026-06-08 08:03:45 +0300

    Add React frontend for Vercel deployment

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <div className="hero">
+        <h1>OMG-PLUS</h1>
+        <p>אמונה. מדע. בינה מלאכותית.</p>
+        <p className="sub">Coming Soon...</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
